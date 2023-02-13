8692d691143c57d2c377370dcba085ad743c92fc
taulo1999 <taulo@live.dk>
2023-02-13 20:55:22 +0100

    commit

--- src/calendar/calandar.js
+++
@@ -1,8 +0,0 @@
-const calendar = [
-    {"january": { days: [1, 2, 3, 4, 5] } },
-    {"february": { days: [1, 2, 3, 4, 5] } },
-    {"march": { days: [1, 2, 3, 4, 5] } },
-    {"april": { days: [1, 2, 3, 4, 5] } },
-]
-
-export { calendar }
