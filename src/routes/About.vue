2cdc8df0aa724c34f0f1e0251efb792e52fc9636
taulo1999 <taulo@live.dk>
2023-03-19 11:27:33 +0100

    about text

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,14 +1,73 @@
 <template>
-    <div class="container w-75 d-flex flex-column align-items-center text-center">
-        <div class="display-6 mb-5">Historien om Ringe & Omegns Biavlerforening (under udvikling)</div>
+    <div class="container d-flex flex-column align-items-center text-center">
+        <div class="display-6 mb-5"> Om Ringe & Omegns Biavlerforening (under udvikling)</div>
+        <div class="row">
+            <div class="col-lg-6">
+                <p>
+                    Velkommen til Ringe & Omegns Biavlerforening! Vi er en passioneret gruppe af biavlere, der er
+                    centreret midt
+                    på Fyn og byder velkommen til medlemmer fra alle fynske egne. Vores mål er at fremme biavlens kunst
+                    og
+                    videnskab og skabe et fællesskab, hvor både nye og erfarne biavlere kan dele deres passion og viden.
+                </p>
+                <p>
+                    Hos os kan du lære alt om praktisk biavl, lige fra hvordan man starter som ny biavler til at avle
+                    dine egne
+                    dronninger. Vi arrangerer også spændende foredrag og besøg hos både hobby- og erhvervsbiavlere, så
+                    du kan
+                    lære fra de bedste og se biavl i praksis. Vores udstillinger er en anden fantastisk måde at lære
+                    mere om
+                    biavl og få inspiration fra andre medlemmers arbejde.
+                </p>
+                <p>
+                    Vores klubhus og skolebigård ligger i det smukke Søllinge, på Sdr. Højrupvejen 104, 5750 Ringe. Det
+                    er det
+                    perfekte sted for os at samles og dele vores viden og passion for biavl. Vi er et fællesskab, der er
+                    dedikeret til at lære og vokse sammen.
+                </p>
+                <p>
+                    Hvis du vil være en del af en dynamisk og spændende biavlerforening, så er Ringe & Omegns
+                    Biavlerforening
+                    det helt rigtige sted for dig. Tøv ikke med at kontakte os for mere information, eller endnu bedre,
+                    kom og
+                    besøg os på en af vores næste begivenheder og oplev fællesskabet og glæden ved biavl selv!
+                </p>
+            </div>
+            <div class="col-lg-6">
+                <p>
+                    Hvis du vil være en del af en dynamisk og spændende biavlerforening, så er Ringe & Omegns
+                    Biavlerforening
+                    det helt rigtige sted for dig. Tøv ikke med at kontakte os for mere information, eller endnu
+                    bedre, kom og
+                    besøg os på en af vores næste begivenheder og oplev fællesskabet og glæden ved biavl selv!
+                </p>
+                <p>
+                    Her på vores hjemmeside kan du finde yderligere information om vores kurser, kommende
+                    begivenheder, og
+                    hvordan
+                    du kan blive medlem af vores fællesskab af biavlere. Du vil også finde masser af ressourcer og
+                    inspiration
+                    til
+                    at forbedre dine biavl færdigheder og udvide din viden om biavl.
+                </p>
+                <p>
+                    Vi er begejstrede for at dele vores passion for biavl med dig og ser frem til at have dig som en
+                    del af
+                    vores
+                    fællesskab. Så tøv ikke med at udforske vores hjemmeside og kontakte os, hvis du har spørgsmål
+                    eller ønsker
+                    at
+                    deltage i vores kommende arrangementer
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: "about-route",
-}
-
+    export default {
+        name: "about-route",
+    }
 </script>
 
 <style>
