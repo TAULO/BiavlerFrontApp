853ac327b6ba3e674badbc0b97693cada529c41d
taulo1999 <taulo@live.dk>
2023-03-26 00:42:41 +0100

    add update event and event getter

--- src/services/firebase/db.js
+++ src/services/firebase/db.js
@@ -1,60 +1,97 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, query, where } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBtHuZxVsR36yNaAMOwuzoi-PkZJEAQ7bo",
-    authDomain: "biavlerforening-66d6d.firebaseapp.com",
-    projectId: "biavlerforening-66d6d",
-    storageBucket: "biavlerforening-66d6d.appspot.com",
-    messagingSenderId: "1075343159849",
-    appId: "1:1075343159849:web:467c654f3a5c9cd0ea2238",
-    measurementId: "G-RG0VSSFG70"
-  };
-
-const app = initializeApp(firebaseConfig);
+import {
+  app
+} from './firebaseInit'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  setDoc,
+  doc,
+  query,
+  where
+} from "firebase/firestore";
 
 const db = getFirestore(app)
 const calendarCol = "calendar"
 
 async function addEvent(title, startDate, endDate, description) {
   try {
-    const ref = await addDoc(collection(db, calendarCol), {
-        title,
-        startDate,
-        endDate,
-        description
+    await addDoc(collection(db, calendarCol), {
+      title,
+      startDate,
+      endDate,
+      description
     })
-    console.log("Added event with id: " + ref.id)
-  } catch(e) {
+    console.log("Added event with title: " + title)
+  } catch (e) {
     console.log("Error adding event:", e)
   }
 }
 
 async function deleteEvent(docId) {
-    const docRef = doc(db, calendarCol, docId)
-    await deleteDoc(docRef)
+  const docRef = doc(db, calendarCol, docId)
+  await deleteDoc(docRef)
     .then(() => console.log("document deleted with id: " + docId))
     .catch(e => console.log("Error deleting event:", e))
-    
+}
+
+async function getEvent(docId) {
+  try {
+    const docRef = doc(db, calendarCol, docId)
+    return (await getDoc(docRef)).data()
+  } catch(e) {
+    console.log(e)
+  }
 }
 
 async function getEvents() {
-    const events = []
-    const querySnapshot = await getDocs(collection(db, calendarCol))
-    querySnapshot.forEach((event) => {
-        events.push({...event.data(), id: event.id})
+  const events = []
+  const querySnapshot = await getDocs(collection(db, calendarCol))
+  querySnapshot.forEach((event) => {
+    events.push({
+      ...event.data(),
+      id: event.id
     })
-    return events
+  })
+  return events
+}
+
+async function updateEvent(docId, title, startDate, endDate, description) {
+  try {
+    const docRef = doc(db, calendarCol, docId)
+    await setDoc(docRef, {
+      title,
+      startDate,
+      endDate,
+      description
+    })
+  } catch(e){
+    console.log(e)
+  }
 }
 
 async function searcEventQuery(title) {
-    const q = query(collection(calendarCol, db), where('title', '==', title))
-    const filteredEvents = []
-    const querySnapshot = await getDocs(q)
-    querySnapshot.forEach((event) => {
-        filteredEvents.push({...event.data(), id: event.id})
+  const q = query(collection(calendarCol, db), where('title', '==', title))
+  const filteredEvents = []
+  const querySnapshot = await getDocs(q)
+  querySnapshot.forEach((event) => {
+    filteredEvents.push({
+      ...event.data(),
+      id: event.id
     })
-    return filteredEvents
+  })
+  return filteredEvents
 }
 
-export { addEvent, getEvents, deleteEvent, searcEventQuery }
+
+export {
+  addEvent,
+  getEvents,
+  getEvent,
+  deleteEvent,
+  updateEvent,
+  searcEventQuery
+}
