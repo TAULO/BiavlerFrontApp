26c7c310ec9f98671dc10bb8028fba0cb7818f6f
taulo1999 <taulo@live.dk>
2023-03-26 00:40:25 +0100

    add firebase init file

---
+++ src/services/firebase/firebaseInit.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtHuZxVsR36yNaAMOwuzoi-PkZJEAQ7bo",
+    authDomain: "biavlerforening-66d6d.firebaseapp.com",
+    projectId: "biavlerforening-66d6d",
+    storageBucket: "biavlerforening-66d6d.appspot.com",
+    messagingSenderId: "1075343159849",
+    appId: "1:1075343159849:web:467c654f3a5c9cd0ea2238",
+    measurementId: "G-RG0VSSFG70"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app }
