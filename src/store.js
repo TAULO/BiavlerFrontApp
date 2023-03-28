465386e0f1c1e2d4eb5ab3f03fbb97278fece3d7
taulo1999 <taulo@live.dk>
2023-03-28 21:34:24 +0200

    split store to modules

--- src/store.js
+++ src/store.js
@@ -18,40 +18,183 @@ import {
   updateEvent
 } from './services/firebase/db'
 
-import { getImagesUrl, uploadImages, deleteImage, deleteImages } from './services/firebase/storage';
+import {
+  getImagesUrl,
+  uploadImages,
+  deleteImage,
+  deleteImages
+} from './services/firebase/storage';
 
-const store = createStore({
+// const store = createStore({
+  // state: {
+  //   user: {
+  //     loggedIn: false,
+  //     data: null
+  //   },
+
+  //   events: [{
+  //     title: "",
+  //     startDate: null,
+  //     endDate: null,
+  //     description: "",
+  //     id: ""
+  //   }],
+
+  //   images: [{
+  //     name: "",
+  //     url: ""
+  //   }]
+
+  // },
+  // getters: {
+  //   user(state) {
+  //     return state.user
+  //   },
+
+  //   events(state) {
+  //     return state.events
+  //   },
+
+  //   images(state) {
+  //     return state.images
+  //   }
+  // },
+
+  // mutations: {
+  //   SET_LOGGED_IN(state, value) {
+  //     state.user.loggedIn = value;
+  //   },
+
+  //   SET_USER(state, data) {
+  //     state.user.data = data;
+  //   },
+
+  //   SET_EVENTS(state, data) {
+  //     state.events = data
+  //   },
+
+  //   SET_IMAGES(state, data) {
+  //     state.images = data
+  //   }
+  // },
+
+  // actions: {
+  //   async logIn(context, {
+  //     email,
+  //     password
+  //   }) {
+  //     const response = await logIn(email, password)
+  //     if (response) {
+  //       context.commit('SET_LOGGED_IN', true)
+  //       context.commit('SET_USER', response.user)
+  //       console.log("login succeeded")
+  //     } else {
+  //       throw new Error('Der skete en fejl ved login. Prøv igen')
+  //     }
+  //   },
+
+  //   async logOut(context) {
+  //     await logOut(auth)
+  //     context.commit('SET_LOGGED_IN', false)
+  //     context.commit('SET_USER', null)
+  //     console.log("user logged out")
+  //   },
+
+  //   authChanged(context) {
+  //     onAuthStateChanged(auth, (user) => {
+  //       if (user) {
+  //         context.commit('SET_LOGGED_IN', true)
+  //         context.commit('SET_USER', user)
+  //       } else {
+  //         context.commit('SET_LOGGED_IN', false)
+  //         context.commit('SET_USER', user)
+  //       }
+  //     })
+  //   },
+
+  //   async addEvent(context, {
+  //     title,
+  //     startDate,
+  //     endDate,
+  //     description
+  //   }) {
+  //     addEvent(title, startDate, endDate, description)
+  //     context.commit('SET_EVENTS', await getEvents())
+  //   },
+
+  //   async deleteEvent(context, {
+  //     docId
+  //   }) {
+  //     deleteEvent(docId)
+  //     context.commit('SET_EVENTS', await getEvents())
+  //   },
+
+  //   async updateEvent(context, {
+  //     docId,
+  //     title,
+  //     startDate,
+  //     endDate,
+  //     description
+  //   }) {
+  //     updateEvent(docId, title, startDate, endDate, description)
+  //     context.commit('SET_EVENTS', await getEvents())
+  //   },
+
+  //   async getEvent(context, {
+  //     docId
+  //   }) {
+  //     return getEvent(docId)
+  //   },
+
+  //   async fetchEvents(context) {
+  //     context.commit('SET_EVENTS', await getEvents())
+  //   },
+
+  //   async searcEventQuery(context, {
+  //     title
+  //   }) {
+  //     context.commit('SET_EVENTS', await searcEventQuery(title))
+  //   },
+
+  //   async getImagesUrl(context) {
+  //     context.commit('SET_IMAGES', await getImagesUrl())
+  //   },
+
+  //   async uploadImages(context, {
+  //     files
+  //   }) {
+  //     await uploadImages(files)
+  //     context.commit('SET_IMAGES', await getImagesUrl())
+  //   },
+
+  //   async deleteImage(context, {
+  //     imageName
+  //   }) {
+  //     await deleteImage(imageName)
+  //     context.commit('SET_IMAGES', await getImagesUrl())
+  //   },
+
+  //   async deleteImages(context, {
+  //     images
+  //   }) {
+  //     await deleteImages(images)
+  //     context.commit('SET_IMAGES', await getImagesUrl())
+  //   }
+  // },
+// })
+
+const authModule = {
   state: {
     user: {
       loggedIn: false,
       data: null
     },
-
-    events: [{
-      title: "",
-      startDate: null,
-      endDate: null,
-      description: "",
-      id: ""
-    }],
-
-    images: [{name: "", url: ""}]
-
   },
   getters: {
     user(state) {
       return state.user
     },
-
-    events(state) {
-      return state.events
-    },
-
-    images(state) {
-      return state.images
-    }
   },
-
   mutations: {
     SET_LOGGED_IN(state, value) {
       state.user.loggedIn = value;
@@ -60,16 +203,7 @@ const store = createStore({
     SET_USER(state, data) {
       state.user.data = data;
     },
-
-    SET_EVENTS(state, data) {
-      state.events = data
-    },
-
-    SET_IMAGES(state, data) {
-      state.images = data
-    }
   },
-
   actions: {
     async logIn(context, {
       email,
@@ -103,7 +237,30 @@ const store = createStore({
         }
       })
     },
+  }
+}
 
+const eventsModule = {
+  state: {
+    events: [{
+      title: "",
+      startDate: null,
+      endDate: null,
+      description: "",
+      id: ""
+    }],
+  },
+  getters: {
+    events(state) {
+      return state.events
+    },
+  },
+  mutations: {
+    SET_EVENTS(state, data) {
+      state.events = data
+    },
+  },
+  actions: {
     async addEvent(context, {
       title,
       startDate,
@@ -132,7 +289,9 @@ const store = createStore({
       context.commit('SET_EVENTS', await getEvents())
     },
 
-    async getEvent(context, { docId }) {
+    async getEvent(context, {
+      docId
+    }) {
       return getEvent(docId)
     },
 
@@ -145,26 +304,61 @@ const store = createStore({
     }) {
       context.commit('SET_EVENTS', await searcEventQuery(title))
     },
+  }
+}
 
+const storageModule = {
+  state: {
+    images: [{
+      name: "",
+      url: ""
+    }]
+  },
+  getters: {
+    images(state) {
+      return state.images
+    }
+  },
+  mutations: {
+    SET_IMAGES(state, data) {
+      state.images = data
+    }
+  },
+  actions: {
     async getImagesUrl(context) {
       context.commit('SET_IMAGES', await getImagesUrl())
     },
 
-    async uploadImages(context, { files }) {
+    async uploadImages(context, {
+      files
+    }) {
       await uploadImages(files)
       context.commit('SET_IMAGES', await getImagesUrl())
     },
 
-    async deleteImage(context, { imageName }) {
+    async deleteImage(context, {
+      imageName
+    }) {
       await deleteImage(imageName)
       context.commit('SET_IMAGES', await getImagesUrl())
     },
 
-    async deleteImages(context, { images }) {
+    async deleteImages(context, {
+      images
+    }) {
       await deleteImages(images)
       context.commit('SET_IMAGES', await getImagesUrl())
     }
-  },
+  }
+}
+
+const store = createStore({
+  modules: {
+    authModule,
+    eventsModule,
+    storageModule
+  }
 })
+
 
 export default store
