0c30069a324d049d504b6f7b3537a19a4aa1c4c4
taulo1999 <taulo@live.dk>
2023-03-28 21:34:02 +0200

    minor changes

--- src/routes/Committee.vue
+++ src/routes/Committee.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="container d-flex flex-column align-items-center">
-        <h2 class="display-6 mb-5 text-center">Bestyrrelse (under udvikling)</h2>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur." email="foo@gmail.com" image="Bee.png"></CommitteeMember>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur. Pariatur aliquip elit tempor esse nostrud do cillum dolore. Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur. Pariatur aliquip elit tempor esse nostrud do cillum dolore. Sint reprehenderit qui amet velit duis eiusmod veniam anim ea est." email="foo@gmail.com" image="Bee.png"></CommitteeMember>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="" email="foo@gmail.com" image="Bee.png"></CommitteeMember>
+    <div class="container">
+        <h2 class="display-6 mb-5 text-center d-flex flex-column align-items-center">Bestyrrelse (under udvikling)</h2>
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <CommitteeMember name="Eksempel" role="Bestyrrelsemedlem" email="foo@gmail.com" image="Bee.png">
+                </CommitteeMember>
+            </div>
+        </div>
     </div>
 </template>
 <script>
