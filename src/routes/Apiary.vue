267da9390044fe1af1af9099c38f26a4b7621639
THOMAS TAULO <thomas@sirenia.eu>
2023-03-28 16:03:39 +0200

    add auth modal

--- src/routes/Apiary.vue
+++ src/routes/Apiary.vue
@@ -1,13 +1,63 @@
 <template>
     <div class="container d-flex flex-column align-items-center text-center">
-        <div class="display-6 mb-5">Året i Bigården(under udvikling)</div>
+        <div class="display-6 mb-5">Året i Bigården (under udvikling)</div>
+        <!-- ADMIN -->
+        <!-- <button v-if="user.loggedIn" class="btn btn-warning m-3" data-bs-toggle="modal"
+            data-bs-target="#addCommitteeModal">Tilføj ny bestyrrelsesmedlem
+        </button> -->
+        <!-- modal -->
+        <!-- <div class="modal fade modal-lg" id="addCommitteeModal">
+            <div class="modal-dialog modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="addCommitteeModal">Tilføj ny bestyrrelsesmedlem</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%" id="basic-addon1">Navn</span>
+                            <input type="text" class="form-control" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%" id="basic-addon1">Rolle</span>
+                            <input class="form-control" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%">Bio</span>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%" id="basic-addon1">e-mail</span>
+                            <input type="email" class="form-control" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="files" type="file">
+                        </div>
+                        <div class="d-flex flex-column justify-content-center align-content-center">
+                            <p>Forhåndsvisning:</p>
+                            <div class="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Luk</button>
+                        <button class="btn btn-warning">Tilføj</button>
+                    </div>
+                </div>
+            </div>
+        </div> -->
     </div>
 </template>
 <script>
-export default {
-    name: "apiary-route"
-}
+    export default {
+        name: "apiary-route",
+        computed: {
+            user() {
+                return this.$store.getters.user
+            },
+        },
+    }
 </script>
 <style>
-    
+
 </style>
--- src/routes/Committee.vue
+++ src/routes/Committee.vue
@@ -1,9 +1,58 @@
 <template>
     <div class="container d-flex flex-column align-items-center">
         <h2 class="display-6 mb-5 text-center">Bestyrrelse (under udvikling)</h2>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur." email="foo@gmail.com" image="Bee.png"></CommitteeMember>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur. Pariatur aliquip elit tempor esse nostrud do cillum dolore. Sint ea enim ex velit quis aliqua nulla consequat mollit laboris nulla minim anim pariatur. Pariatur aliquip elit tempor esse nostrud do cillum dolore. Sint reprehenderit qui amet velit duis eiusmod veniam anim ea est." email="foo@gmail.com" image="Bee.png"></CommitteeMember>
-            <CommitteeMember name="Foobar Bar Foo" role="Bestyrelsesmedlem" bio="" email="foo@gmail.com" image="Bee.png"></CommitteeMember>
+        <!-- ADMIN -->
+        <button v-if="user.loggedIn" class="btn btn-warning m-3" data-bs-toggle="modal"
+            data-bs-target="#addCommitteeModal">Tilføj ny bestyrrelsesmedlem
+        </button>
+        <!-- modal -->
+        <div class="modal fade modal-lg" id="addCommitteeModal">
+            <div class="modal-dialog modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="addCommitteeModal">Tilføj ny bestyrrelsesmedlem</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%"  id="basic-addon1">Navn</span>
+                            <input type="text" class="form-control"
+                                aria-describedby="basic-addon1" v-model="committeeMember.name">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%" id="basic-addon1">Rolle</span>
+                            <input class="form-control"
+                            aria-describedby="basic-addon1" v-model="committeeMember.role">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%" >Bio</span>
+                            <textarea class="form-control" v-model="committeeMember.bio"></textarea>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" style="min-width: 10%"  id="basic-addon1">e-mail</span>
+                            <input type="email" class="form-control"
+                            aria-describedby="basic-addon1" v-model="committeeMember.email">
+                        </div>
+                        <div class="input-group mb-3">
+                            <!-- <label class="input-group-text btn btn-secondary border-1" for="files">vælg billede</label> -->
+                            <input id="files" type="file" @change="getFilesOnChange($event)" >
+                        </div>
+                        <div class="d-flex flex-column justify-content-center align-content-center">
+                            <p>Forhåndsvisning:</p>
+                            <div class="">
+                                <CommitteeMember :name=this.committeeMember.name :role="this.committeeMember.role" :bio="this.committeeMember.bio" :email="this.committeeMember.email" :image=imgSrc></CommitteeMember>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Luk</button>
+                        <button class="btn btn-warning" @click="addCommitteeMember()">Tilføj</button>
+                        <!-- <button class="btn btn-warning">Ændre</button> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <CommitteeMember name="Eksempel" role="Bestyrelsesmedlem" bio="Bio" email="foo@gmail.com" image="Bee.png"></CommitteeMember>
     </div>
 </template>
 <script>
@@ -11,8 +60,38 @@
 
     export default {
         name: "committee-route",
+
+        data() {
+            return {
+                committeeMember: {
+                    name: "",
+                    role: "",
+                    bio: "",
+                    email: "",
+                    imgFile: "",
+                    id: ""
+                },
+            }
+        },
+
         components: {
             CommitteeMember
+        },
+        
+        computed: {
+            user() {
+                return this.$store.getters.user
+            },
+        },
+
+        methods: {
+            addCommitteeMember() {
+                console.log(this.committeeMember)
+            },
+
+            getFilesOnChange(event) {
+                this.committeeMember.imgFile = event.target.files[0]
+            }
         }
     }
 </script>
